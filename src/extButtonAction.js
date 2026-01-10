f87cac4817b40b153a78b0c5ec7dbf3bd8b62620
Kira <38155247+Nyanraltotlapun@users.noreply.github.com>
2026-01-10 21:28:28 +0100

    Migration to manifest v3 and chrome compatibility.

--- src/extButtonAction.js
+++ src/extButtonAction.js
@@ -1,12 +1,18 @@
 /*
 Open a new tab, and load "window.html" into it.
 */
+
+// Workaround for chrome based browsers
+if (typeof browser === "undefined") {
+  var browser = chrome;
+}
+
 function openWindow() {
   console.log("injecting");
    browser.tabs.create({
-     "url": "/window/window.html"
+     "url": "/window/window.html",
    });
 }
 
+browser.action.onClicked.addListener(openWindow);
 
-browser.browserAction.onClicked.addListener(openWindow);
