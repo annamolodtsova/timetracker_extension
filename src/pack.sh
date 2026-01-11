4b2d503c4cfd4feaa3c5fa802aa7f7db8c7040b9
Kira <38155247+Nyanraltotlapun@users.noreply.github.com>
2026-01-11 15:21:39 +0100

    Made separete manifests for chrome and firefox, and improve packing script.

--- src/pack.sh
+++ src/pack.sh
@@ -2,10 +2,20 @@
 
 mkdir ../dist
 
-zip -9 -r ../dist/timetracker_extension.xpi ./extension/
+cp -r ./extension ../dist/
+cd ../dist
+mv ./extension/manifest_chrome.json ./
+mv ./extension/manifest_firefox.json ./extension/manifest.json
+
+(cd ./extension && zip -9 -r ../timetracker_extension.xpi *)
+
+mv manifest_chrome.json ./extension/manifest.json
 
 # brave --pack-extension=extension --pack-extension-key=/path/to/key
 brave --pack-extension=extension
-mv extension.crx ../dist/timetracker_extension.crx
-mv extension.pem ../dist/timetracker_extension.pem
+mv extension.crx timetracker_extension.crx
+mv extension.pem timetracker_extension.pem
+
+rm -r extension
+
 
